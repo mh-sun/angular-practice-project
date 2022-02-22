e8bfdd133ba29fe71719356e70fcd01c0876ebae
Mehedi Hasan Sun <bsse1025@iit.du.ac.bd>
2022-02-22 22:52:52 +0600

    Manual Component generate

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { TestComponent } from './test/test.component';
-import { TestChildComponent } from './test-child/test-child.component';
 import { FormsModule } from '@angular/forms';
+import { ParentComponent } from './parent/parent.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TestComponent,
-    TestChildComponent
+    ParentComponent
   ],
   imports: [
     BrowserModule,
