52881703b16b7d981388b6932d76a1a8dc4d52a2
Mehedi Hasan Sun <bsse1025@iit.du.ac.bd>
2022-02-23 17:07:37 +0600

    component lifecycle hooks

--- src/app/parent/parent.component.ts
+++ src/app/parent/parent.component.ts
@@ -1,7 +1,32 @@
-import { Component } from "@angular/core";
+import { Component, DoCheck, OnChanges, OnDestroy, OnInit, SimpleChanges } from "@angular/core";
 
 @Component({
-    selector: 'app-parent',
+    // selector: 'app-parent',
+    // selector: '.app-parent',
+    selector: "app-parent",
     templateUrl: "./parent.component.html"
 })
-export class ParentComponent{}
+export class ParentComponent implements 
+    OnInit, 
+    OnChanges, 
+    DoCheck,
+    OnDestroy
+{
+    ngDoCheck(): void {
+        console.log("DoCheck called")
+    }
+    ngOnChanges(changes: SimpleChanges): void {
+        console.log("OnChange called")
+    }
+    ngOnInit(): void {
+        console.log("OnInit called")
+    }
+    ngOnDestroy(): void {
+        console.log("onDestroy called")
+    }
+    name:string = 'Mehedi Hasan Sun'
+    isShown:boolean = true
+    toggle(){
+        this.isShown = !this.isShown
+    }
+}
